7508f838099999cf6bc357c61237b2278fc384b2
alcav <71708992+alcav@users.noreply.github.com>
2020-10-24 15:44:35 +0200

    Add files via upload

--- funktionen.py
+++ funktionen.py
@@ -1,3 +1,12 @@
 import json
 
-#hier kommen die Funktionen, damit sich die Daten abspeichern lassen
+
+def eingabe_speichern(datum, aufgabe, anfangszeit, endzeit, pause):
+    try:
+        with open("zeiterfassung.txt", "r") as open_file:
+            zeiterfassung = json.load(open_file)
+
+    except FileNotFoundError:
+        zeiterfassung = []
+
+    datum = ('aufgabe', 'anfangszeit', 'endzeit', 'pause')  # Das ist ein Tuple = eine unveränderbare Liste.
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template, request
 import json
-import funktionen    #noch nicht genutzt.........
+import funktionen
 
 
 app = Flask("TimeTool")
 
 
-@app.route('/', methods=['POST', 'GET'])
-def home():
+@app.route('/', methods=['GET', 'POST'])
+def speichern():
+    if request_method == 'POST':
+        datum = request.form['datum']
+        aufgabe = request.form['aufgabe']
+        startzeit = request.form['startzeit']
+        endzeit = request.form['endzeit']
+        pause = request.form['pause']
+        funktionen.eingabe_speichern(datum, aufgabe, startzeit, endzeit, pause)
     return render_template('index.html')
-# Hier kommt eine Funktion, die in den Funktionen definiert wird.........
+
+# Hier wird eine Funktion ausgeführt, die in den Funktionen definiert wird.........
 # Damit sollen die Einträge als dict in json abgespeichert werden können .........
 
 
@@ -25,4 +33,3 @@ def grafiken():
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
